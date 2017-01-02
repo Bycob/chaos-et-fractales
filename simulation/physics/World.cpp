8b8beadb44e027034239e1ed391598b1de0a4773
BynaryCobweb <ljean@etud.insa-toulouse.fr>
2017-01-02 14:24:26 +0100

    Commentaires sur la classe World

--- simulation/physics/World.cpp
+++ simulation/physics/World.cpp
@@ -23,6 +23,8 @@ void World::addObject(std::shared_ptr<Body> body) {
 vec3d World::getSystemLinearMomentum() {
     vec3d result(0.0, 0.0, 0.0);
 
+	// On somme les quantités de mouvement (c'est à dire la masse multipliée
+	// par la vitesse) de tous les corps.
     for (auto &body : bodies) {
         vec3d speed(body->vx, body->vy, body->vz);
         result += speed * body->mass;
@@ -34,10 +36,13 @@ vec3d World::getSystemLinearMomentum() {
 vec3d World::getSystemPosition() {
     vec3d result(0.0, 0.0, 0.0);
 
+	// On calcule la moyenne des positions, égale à la somme de la position de tous
+	// les corps du système, divisée par le nombre de corps.
     for (auto &body : bodies) {
         vec3d position(body->x, body->y, body->z);
         result += position;
     }
+	result /= bodies.size();
 
     return result;
 }
@@ -46,7 +51,7 @@ void World::step(double seconds, int multiplier) {
     time += seconds * multiplier;
 
     for (int i = 0 ; i < multiplier ; i++) {
-        //CALCUL DES NOUVELLES VARIABLES POUR TOUS LES CORPS
+        // On applique la méthode selectionnée par l'utilisateur.
         switch (method) {
         case Method::EULER :
             euler(seconds);
@@ -61,101 +66,69 @@ void World::step(double seconds, int multiplier) {
 void World::euler(double t) {
 
     for (auto &body : this->bodies) {
+		// Pour chaque corps on calcule l'accéleration au temps actuel
         body->ax = 0;
         body->ay = 0;
         body->az = 0;
 
+		// L'accéleration est la somme des vecteurs force de gravitation
+		// exercée par chaque corps du système sur le corps étudié.
         for (auto &body2 : this->bodies) {
             if (body != body2) {
+				// On précalcule la distance entre les deux corps et le produit
+				// m * g / d^3
                 double length = body->distance(*body2.get());
                 if (length == 0) continue;
 
-                double mg = G * body2->mass;
+                double k = G * body2->mass / (length * length * length);
 
-                body->ax += mg * (body2->x - body->x) / (length * length * length);
-                body->ay += mg * (body2->y - body->y) / (length * length * length);
-                body->az += mg * (body2->z - body->z) / (length * length * length);
+				// On ajoute les trois composantes du vecteur force aux trois composantes
+				// de l'acceleration.
+                body->ax += k * (body2->x - body->x);
+                body->ay += k * (body2->y - body->y);
+                body->az += k * (body2->z - body->z);
             }
         }
 
-        body->x += body->vx * t + body->ax * t * t;
-        body->y += body->vy * t + body->ay * t * t;
-        body->z += body->vz * t + body->az * t * t;
+		// On intègre la vitesse pour trouver la position finale
+		// (sachant que v1 = v0 + a0 * t, avec v1 vitesse finale, v0 et a0
+		// vitesse et accéleration initiales)
+        body->x += body->vx * t + body->ax * t * t / 2;
+        body->y += body->vy * t + body->ay * t * t / 2;
+        body->z += body->vz * t + body->az * t * t / 2;
 
+		// On intègre l'accéleration pour trouver la vitesse finale
         body->vx += body->ax * t;
         body->vy += body->ay * t;
         body->vz += body->az * t;
     }
 }
 
-
-mat3x4d World::rungekuttaFunc3Bodies(mat3x4d & in) {
-    mat3x4d out(
-            in[0][2], in[0][3], 0, 0,
-            in[1][2], in[1][3], 0, 0,
-            in[2][2], in[2][3], 0, 0
-    );
-
-    for (int i = 0 ; i < 3 ; i++) {
-        for (int j = 0 ; j < 3 ; j++) {
-            if (i == j) continue;
-
-            double length = bodies[i]->distance(*bodies[j]);
-            if (length == 0) continue;
-
-            out[i][2] = out[i][2] + G * bodies[j]->mass * (in[j][0] - in[i][0]) / (length * length * length);
-            out[i][3] = out[i][3] + G * bodies[j]->mass * (in[j][1] - in[i][1]) / (length * length * length);
-        }
-    }
-
-    return out;
-}
-
-void World::rungekutta3Bodies(double t) {
-    if (bodies.size() != 3) return;
-
-    mat3x4d M(bodies[0]->x, bodies[0]->y, bodies[0]->vx, bodies[0]->vy,
-                  bodies[1]->x, bodies[1]->y, bodies[1]->vx, bodies[1]->vy,
-                  bodies[2]->x, bodies[2]->y, bodies[2]->vx, bodies[2]->vy);
-
-    auto k1 = rungekuttaFunc3Bodies(M);
-    auto M2 = M + k1 * (t / 2);
-    auto k2 = rungekuttaFunc3Bodies(M2);
-    auto M3 = M + k2 * (t / 2);
-    auto k3 = rungekuttaFunc3Bodies(M3);
-    auto M4 = M + k3 * t;
-    auto k4 = rungekuttaFunc3Bodies(M4);
-
-    bodies[0]->x += (t / 6) * (k1[0][0] + 2 * k2[0][0] + 2 * k3[0][0] + k4[0][0]);
-    bodies[0]->y += (t / 6) * (k1[0][1] + 2 * k2[0][1] + 2 * k3[0][1] + k4[0][1]);
-    bodies[0]->vx += (t / 6) * (k1[0][2] + 2 * k2[0][2] + 2 * k3[0][2] + k4[0][2]);
-    bodies[0]->vy += (t / 6) * (k1[0][3] + 2 * k2[0][3] + 2 * k3[0][3] + k4[0][3]);
-
-    bodies[1]->x += (t / 6) * (k1[1][0] + 2 * k2[1][0] + 2 * k3[1][0] + k4[1][0]);
-    bodies[1]->y += (t / 6) * (k1[1][1] + 2 * k2[1][1] + 2 * k3[1][1] + k4[1][1]);
-    bodies[1]->vx += (t / 6) * (k1[1][2] + 2 * k2[1][2] + 2 * k3[1][2] + k4[1][2]);
-    bodies[1]->vy += (t / 6) * (k1[1][3] + 2 * k2[1][3] + 2 * k3[1][3] + k4[1][3]);
-
-    bodies[2]->x += (t / 6) * (k1[2][0] + 2 * k2[2][0] + 2 * k3[2][0] + k4[2][0]);
-    bodies[2]->y += (t / 6) * (k1[2][1] + 2 * k2[2][1] + 2 * k3[2][1] + k4[2][1]);
-    bodies[2]->vx += (t / 6) * (k1[2][2] + 2 * k2[2][2] + 2 * k3[2][2] + k4[2][2]);
-    bodies[2]->vy += (t / 6) * (k1[2][3] + 2 * k2[2][3] + 2 * k3[2][3] + k4[2][3]);
-}
-
 Matrixdv World::rungekuttaFuncNBodies(Matrixdv &in) {
+	// Matrice qui contient le résultat
     Matrixdv result(6, this->bodies.size());
 
+	// On calcule la matrice pour chaque corps i du monde, auquel correspond
+	// la ligne i de la matrice d'entrée.
     for (int i = 0 ; i < this->bodies.size() ; i++) {
+		// Les trois premières colonnes de la matrice résultat contiennent la
+		// vitesse du corps.
         result[i][0] = in[i][3];
         result[i][1] = in[i][4];
         result[i][2] = in[i][5];
 
+		// Les trois colonnes suivantes contiennent son accélération, égale à la
+		// somme de tous les vecteurs force de gravitation exercé par chaque corps
+		// j sur le corps i.
         for (int j = 0 ; j < this->bodies.size() ; j++) {
             if (i == j) continue;
 
+			// On récupère la distance qui sépare les deux corps
             double length = bodies[i]->distance(*bodies[j]);
-            if (length == 0) continue;
+			// Si la distance est trop petite on risque une division par 0
+            if (length < 0.000000001) continue;
 
+			// On ajoute le vecteur force de gravitation au résultat.
             result[i][3] = result[i][3] + G * bodies[j]->mass * (in[j][0] - in[i][0]) / (length * length * length);
             result[i][4] = result[i][4] + G * bodies[j]->mass * (in[j][1] - in[i][1]) / (length * length * length);
             result[i][5] = result[i][5] + G * bodies[j]->mass * (in[j][2] - in[i][2]) / (length * length * length);
@@ -167,7 +140,8 @@ Matrixdv World::rungekuttaFuncNBodies(Matrixdv &in) {
 
 void World::rungekuttaNBodies(double t) {
 
-    //Matrification
+    // On place les coordonnées et la vitesse de chaque corps dans une matrice
+	// de 6 colonnes et n lignes, avec n le nombre de corps.
     Matrixdv M(6, this->bodies.size());
     for (int i = 0 ; i < this->bodies.size() ; i++) {
         auto body = this->bodies[i];
@@ -179,7 +153,8 @@ void World::rungekuttaNBodies(double t) {
         M[i][5] = body->vz;
     }
 
-    //Runge kutta
+    // On applique la méthode de Runge-Kutta sur la matrice ainsi créée.
+	// On calcule les matrices k1, k2, k3 et k4.
     auto k1 = rungekuttaFuncNBodies(M);
     auto M2 = M + k1 * (t / 2);
     auto k2 = rungekuttaFuncNBodies(M2);
@@ -188,7 +163,8 @@ void World::rungekuttaNBodies(double t) {
     auto M4 = M + k3 * t;
     auto k4 = rungekuttaFuncNBodies(M4);
 
-    //Dematrification
+    // A partir des matrices k1, k2, k3 et k4, on retrouve les positions des
+	// corps au bout du temps t.
     for (int i = 0 ; i < this->bodies.size() ; i++) {
         auto & body = this->bodies[i];
         body->x += (t / 6) * (k1[i][0] + 2 * k2[i][0] + 2 * k3[i][0] + k4[i][0]);
--- simulation/physics/World.h
+++ simulation/physics/World.h
@@ -31,38 +31,59 @@ public :
 
     virtual ~World() {};
 
+	/** Définit la méthode pour calculer les nouvelles positions des corps
+	lorsqu'on appelle la méthode #step(double, int) */
     void setMethod(const Method & method);
     void addObject(std::shared_ptr<Body> body);
 
+	/** Définit la constante universelle de gravitation qui sera utilisée pour
+	les calculs */
     void setGravityConstant(double g) { this->G = g;}
     double getGravityConstant() {return G;}
 
     double getTime() {return time;}
+	/**@returns La quantité de mouvement totale du système, qui est la somme
+	des quantités de mouvement de chacun des corps */
     vec3d getSystemLinearMomentum();
+	/** @returns La position du système, soit la moyenne des positions de tous
+	les corps. */
     vec3d getSystemPosition();
 
-    ///Effectue la simulation sur le nombre de secondes passé en paramètre,
-    /// multiplié par le second paramètre de la fonction.
+    /** Calcule les nouvelles variables du système après un certain temps.
+	Cette fonction peut calculer les nouvelles variables au bout de plusieurs
+	pas, il suffit pour cela de mettre un multiplier différent de 1.
+	Exemple : step(0.5, 10) calcule l'état du monde au bout de 5 secondes, en
+	utilisant un pas de 0.5 seconde.
+	@param seconds Le pas physique, utilisé par les méthodes de Runge-Kutta
+	et Euler. Le pas peut être négatif, dans ce cas le temps s'écoulera en
+	sens inverse.
+	@param multiplier Le nombre de pas à calculer.*/
     void step(double seconds, int multiplier);
 private :
     //Différentes méthodes de calcul
+
+	/** Calcule l'état du monde au bout du temps t, en appliquant la méthode
+	d'Euler. */
     void euler(double t);
 
-    mat3x4d rungekuttaFunc3Bodies(mat3x4d & in);
-    void rungekutta3Bodies(double t);
-
+	/** Fonction utilisée pour calculer les matrices k1, k2, k3 et k4 lorsqu'on
+	utilise la méthode de Runge-Kutta.*/
     Matrixdv rungekuttaFuncNBodies(Matrixdv & in);
+	/** Calcule l'état du monde au bout du temps t, en appliquant la méthode
+	de Runge-Kutta */
     void rungekuttaNBodies(double t);
 
 
+	/// Vector contenant l'ensemble des corps du monde.
     std::vector<std::shared_ptr<Body>> bodies;
 
-    ///Ce champs contient la valeur de la constante universelle de gravitation.
+    /// Ce champ contient la valeur de la constante universelle de gravitation.
     double G = GRAVITY_CONSTANT;
 
     /// Ce champ comptabilise le temps écoulé depuis la naissance du monde.
     double time;
 
+	/// La méthode utilisée pour les calculs
     Method method = Method::EULER;
 };
 
